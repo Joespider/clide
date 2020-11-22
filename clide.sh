f4c0826dcff410db3ee42422af707eeca3c73eb5
joespider <coffmandj@gmail.com>
2020-11-21 20:57:27 -0500

    Added modes; added repo mode

--- clide.sh
+++ clide.sh
@@ -37,6 +37,7 @@ repoAssist=$(GetConfig repoAssist)
 #root dir
 ProgDir=$(eval echo $(GetConfig ProgDir))
 ClideDir=${ProgDir}/.clide
+ModesDir=${ClideDir}/modes
 NotesDir=${ClideDir}/notes
 LibDir=${ClideDir}/lib
 LangsDir=${ClideDir}/langs
@@ -232,6 +233,24 @@ ManageLangs()
 	else
 		UseOther ${Langs} ${Manage[@]}
 	fi
+}
+
+ModeHanlder()
+{
+	local Mode=$1
+	case ${Mode} in
+		${repoTool}|repo)
+			#Use ONLY for Projects
+			if [[ ! "${CodeProject}" == "none" ]]; then
+				${ModesDir}/repo.sh ${repoTool} ${CodeProject}
+			else
+				echo "Must have an active project"
+			fi
+			;;
+		*)
+			echo "Modes"
+			;;
+	esac
 }
 
 EnsureDirs()
@@ -503,7 +522,8 @@ newProject()
 		#Language Value
 		echo "lang=${lang}" >> ${ProjectFile}
 		#Create Project and get path
-		path=$(ManageLangs ${Lang} "newProject")
+		path=$(ManageLangs ${Lang} "newProject" ${project})
+		cd ${path}
 		#Path Value
 		echo "path=${path}" >> ${ProjectFile}
 		#Source Value
@@ -728,241 +748,6 @@ ColorCodes()
 	echo ${ChosenLangs}
 }
 
-#Handle Git commands
-gitHandler()
-{
-	local repoAct=$1
-	shift
-	#check if git is installed
-	GitTool=$(which git)
-	#Git is installed
-	if [ ! -z "${GitTool}" ]; then
-		case ${repoAct} in
-			#Create a new repo
-			new|init)
-				echo git init
-				;;
-			#clone a new repo
-			setup|clone)
-				#Find repo name
-				repo=$@
-				if [ ! -z "${repo}" ]; then
-					echo git clone ${repo[@]}
-				#Repo not given
-				else
-					#Ask User for Repo
-					echo -n "repo: "
-					read -a repo
-					#Repo given
-					if [ ! -z "${repo}" ]; then
-						#Run through 2nd time
-						gitHandler "clone" "${repo[@]}"
-					#Again...nothing
-					else
-						#Nothing to do
-						echo "Nothing to clone"
-					fi
-				fi
-				;;
-			#Add files to changes
-			add)
-				files=$@
-				#Files given
-				if [ ! -z "${files}" ]; then
-					echo git add ${files}
-				else
-					#Get ALL files from user
-					echo git add .
-				fi
-				;;
-			#Provide message for repo
-			message|commit)
-				#Get message
-				msg=$@
-				if [ ! -z "${msg}" ]; then
-					echo git commit -m "\"${msg}\""
-				#No message found
-				else
-					#As for user...get EVERYTHING typed
-					echo -n "Message: "
-					read -a msg
-					#Message given
-					if [ ! -z "${msg}" ]; then
-						gitHandler "commit" "${msg[@]}"
-					else
-						echo "No message found"
-					fi
-				fi
-				;;
-			#Handles Git Branches
-			branch|branches)
-				branchAct=$1
-				shift
-				case ${branchAct} in
-					new)
-						name=$1
-						if [ ! -z "${name}" ]; then
-							echo git checkout -b "${name}"
-						else
-							echo -n "Provide a branch name"
-							read name
-							if [ ! -z "${name}" ]; then
-								gitHandler "branch" "new" "${name}"
-							else
-								echo "No branch has been created"
-							fi
-						fi
-						;;
-					#delete branches on local repo
-					remove|delete)
-						#Get branch name
-						name=$1
-						#branch name given
-						if [ ! -z "${name}" ]; then
-							#remove branch
-							echo git branch -d "${name}"
-						#no branch name given
-						else
-							#Get user to type branch name
-							echo -n "Provide a branch name"
-							read name
-							#branch name given
-							if [ ! -z "${name}" ]; then
-								#remove branch
-								gitHandler "branch" "delete" "${name}"
-							#no branch name given
-							else
-								echo "No Branch has been deleted"
-							fi
-						fi
-						;;
-					select|checkout)
-						#Get branch name
-						name=$1
-						#branch name given
-						if [ ! -z "${name}" ]; then
-							#Select branch
-							echo git checkout "${name}"
-						#no branch name given
-						else
-							#Get user to type branch name
-							echo -n "Provide a branch name"
-							read name
-							#branch name given
-							if [ ! -z "${name}" ]; then
-								#Select branch
-								gitHandler "branch" "checkout" "${name}"
-							#no branch name given
-							else
-								echo "No Branch has been selected"
-							fi
-						fi
-						;;
-					#list all branches
-					*)
-						echo git branch -a
-						;;
-				esac
-				;;
-			upload|push)
-				branch=$1
-				if [ ! -z "${branch}" ]; then
-					echo git push origin "\"${branch}\""
-				else
-					echo -n "Please choose a banch: "
-					read branch
-					if [ ! -z "${branch}" ]; then
-						gitHandler "push" "${branch}"
-					else
-						echo "Code not pushed"
-					fi
-				fi
-				;;
-			#Download from the repo
-			download|pull)
-				echo git pull
-				;;
-			#Display repo infortmation
-			state|status)
-				echo git status
-				;;
-			#Peform quick and dirty commit
-			slamdunk)
-				gitHandler "add"
-				gitHandler "commit"
-				gitHandler "push"
-				;;
-			help|options)
-				echo "git help page"
-				;;
-			*)
-				RepoVersion
-				;;
-		esac
-	#git is not installed
-	else
-		echo "Please Install git"
-	fi
-}
-
-svnHandler()
-{
-	local repoAct=$1
-	shift
-	#check if git is installed
-	SvnTool=$(which svn)
-	#Git is installed
-	if [ ! -z "${SvnTool}" ]; then
-		echo "svn is installed"
-	#svn is not installed
-	else
-		echo "Please Install svn"
-	fi
-}
-
-repoHandler()
-{
-	case ${repoTool} in
-		git)
-			#git execution is handled by user
-			if [[ "${repoAssist}" == "False" ]] && [[ "$1" == "${repoTool}" ]]; then
-				IsInstalled=$(which ${repoTool})
-				if [ ! -z "${IsInstalled}" ]; then
-					$@
-				else
-					echo "\"${repoTool}\" is not installed"
-				fi
-			#git execution is handled by cl[ide]
-			elif [[ "${repoAssist}" == "True" ]]; then
-				shift
-				gitHandler $@
-			else
-				echo "repo version control has been disabled"
-			fi
-			;;
-		svn)
-			#svn execution is handled by user
-			if [[ "${repoAssist}" == "False" ]] && [[ "$1" == "${repoTool}" ]]; then
-				IsInstalled=$(which ${repoTool})
-				if [ ! -z "${IsInstalled}" ]; then
-					$@
-				else
-					echo "\"${repoTool}\" is not installed"
-				fi
-			#svn execution is handled by cl[ide]
-			elif [[ "${repoAssist}" == "True" ]]; then
-				shift
-				svnHandler $@
-			else
-				echo "repo version control has been disabled"
-			fi
-			;;
-		*)
-			echo "${Head} is unable to use \"${repoTool}\" at this time"
-			;;
-	esac
-}
-
 #IDE
 Actions()
 {
@@ -1011,20 +796,19 @@ Actions()
 				else
 					ThePWD=$(pwd)
 					ProjectDir=$(echo ${ThePWD#*${CodeProject}} | sed "s/\//:/1")
-					cCodeProject=$(echo -e "\e[1;40m${CodeProject}\e[0m")
+					cCodeProject=$(ManageLangs ${Lang} "ProjectColor" "${CodeProject}")
 					#Menu with no code
-					prompt="${Name}(${cLang}[${cCodeProject}${ClideProjectDir}]):$ "
+					prompt="${Name}(${cLang}[${cCodeProject}${ProjectDir}]):$ "
 				fi
 			else
 				if [[ "${CodeProject}" == "none" ]]; then
 					#Menu with code
-					cCodeProject=$(echo -e "\e[1;40m${CodeProject}\e[0m")
 					prompt="${Name}(${cLang}{${cCode}}):$ "
 				else
 					ThePWD=$(pwd)
 					ProjectDir=$(echo ${ThePWD#*${CodeProject}} | sed "s/\//:/1")
 					#Menu with no code
-					cCodeProject=$(echo -e "\e[1;40m${CodeProject}\e[0m")
+					cCodeProject=$(ManageLangs ${Lang} "ProjectColor" "${CodeProject}")
 					prompt="${Name}(${cLang}[${cCodeProject}${ClideProjectDir}]{${cCode}}):$ "
 				fi
 			fi
@@ -1113,10 +897,9 @@ Actions()
 									newProject ${Lang} ${UserIn[2]}
 									Code=""
 									updateProject ${UserIn[2]} ${Code}
-									if [ ! -z ${UserIn[2]} ]; then
+									if [ ! -z "${UserIn[2]}" ]; then
 										CodeProject=${UserIn[2]}
 										echo "Created \"${CodeProject}\""
-										ThePWD=$(pwd)
 										ProjectDir=$(echo ${ThePWD#*${CodeProject}} | sed "s/\//:/1")
 									fi
 								fi
@@ -1280,14 +1063,9 @@ Actions()
 						echo "${mode} (src|bin)"
 					fi
 					;;
-				#git/svn handler
-				${repoTool}|repo)
-					#Use ONLY for Projects
-					if [[ ! "${CodeProject}" == "none" ]]; then
-						repoHandler ${UserIn[@]}
-					else
-						echo "Must have an active project"
-					fi
+				#Modes
+				mode)
+					ModeHanlder ${UserIn[1]}
 					;;
 				#search for element in project
 				search)
